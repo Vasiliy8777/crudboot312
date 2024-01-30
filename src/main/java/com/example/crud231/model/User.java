8aec5635acd2db37e033800b058b4e7f828239db
Vasiliy <exs777@mail.ru>
2024-01-30 13:26:10 +0300

    commit 4

--- src/main/java/com/example/crud231/model/User.java
+++ src/main/java/com/example/crud231/model/User.java
@@ -5,12 +5,10 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
 import lombok.Data;
 
-
 @Data
 @Entity
 @Table(name = "users")
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -23,62 +21,10 @@ public class User {
     @Size(min=2, max=50,message = "Surname is too long")
     private String surname;
     @Column
-    @NotBlank(message = "Age should not be empty")
     @Min(value = 0, message = "Age should be greater than 0")
     private int age;
     @Column
     @Email(message = "Email should not be valid")
-    @NotEmpty(message = "Email should not be empty")
+    @NotBlank(message = "Email should not be empty")
     private String email;
-
-//    public User() {
-//    }
-//
-//    public User(String name, String surname, int age, String email) {
-//        this.name = name;
-//        this.surname = surname;
-//        this.age = age;
-//        this.email = email;
-//    }
-//
-//    public int getAge() {
-//        return age;
-//    }
-//
-//    public void setAge(int age) {
-//        this.age = age;
-//    }
-//
-//    public String getEmail() {
-//        return email;
-//    }
-//
-//    public void setEmail(String email) {
-//        this.email = email;
-//    }
-//
-//    public int getId() {
-//        return id;
-//    }
-//
-//    public void setId(int id) {
-//        this.id = id;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    public String getSurname() {
-//        return surname;
-//    }
-//
-//    public void setSurname(String surname) {
-//        this.surname = surname;
-//    }
-
 }
